756faa250bd60fe0a99a70e089b610430fd4a9b6
Viktor <viktor.egorov@triology.de>
2026-07-08 13:53:27 +0200

    Change scm minimum Version to 3.0

--- src/main/java/com/cloudogu/scm/metrics/PrometheusMonitoringSystem.java
+++ src/main/java/com/cloudogu/scm/metrics/PrometheusMonitoringSystem.java
@@ -24,7 +24,7 @@ import sonia.scm.metrics.MonitoringSystem;
 import sonia.scm.metrics.ScrapeTarget;
 import sonia.scm.plugin.Extension;
 
-import javax.inject.Singleton;
+import jakarta.inject.Singleton;
 import java.util.Optional;
 
 @Extension
